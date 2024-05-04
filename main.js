2bd6741327d9b2f7c451347dc127cd2e168f2e48
Domegal13 <Domegal13cursos@gmail.com>
2024-05-04 15:29:21 -0300

    Se agregó la clase sticky

--- main.js
+++ main.js
@@ -7,18 +7,3 @@ window.addEventListener("scroll", () => {
   header.classList.toggle("sticky", this.window.scrollY > 80);
   navIcons.classList.toggle("sticky", this.window.scrollY > 80);
 });
-
-//  function validateForm() {
-//    let isValid = true;
-//    const form = document.getElementById("contactForm");
-//    const inputs = form.querySelectorAll("input, select, textarea");
-//    inputs.forEach((input) => {
-//      if (input.required && !input.value.trim()) {
-//        isValid = false;
-//        input.classList.add("error");
-//      } else {
-//        input.classList.remove("error");
-//      }
-//    });
-//    return isValid;
-//  }
