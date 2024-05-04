0010ba7b8d0f071fd9fec73429314ea5da1530d2
Domegal13 <Domegal13cursos@gmail.com>
2024-05-04 15:30:42 -0300

    Se agregó la clase sticky JS

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ const header = document.querySelector("header");
 const navIcons = document.querySelector(".nav-icons");
 
 window.addEventListener("scroll", () => {
-  header.classList.toggle("sticky", this.window.scrollY > 80);
-  navIcons.classList.toggle("sticky", this.window.scrollY > 80);
+  header.classList.toggle("sticky", window.scrollY > 80);
+  navIcons.classList.toggle("sticky", window.scrollY > 80);
 });
